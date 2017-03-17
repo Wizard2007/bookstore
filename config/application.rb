d65456e1901cd12cd5b688906c0d61d6af7fc485
Eujine <sponsor85@mail.ru>
2017-03-17 14:25:02 +0200

    [*] fix render via hamle

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module Bookstore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded.    
   end
+end
+
+# Default template engine for Rails Generators. Use HAML instead of ERB.
+config.generators do |g|
+  g.template_engine :haml
 end
