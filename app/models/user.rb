103a40335a68b67b023bc660b1d434138b75ca29
Eujine <sponsor85@mail.ru>
2017-03-17 10:23:06 +0200

    [*] fix user model for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,5 @@ class User < ApplicationRecord
   has_many :user_adresses
   has_many :reviews
   validates :first_name, :password, :confirmation, presence: true
-  validates :email
-  =begin , length: {
-      minimum: 1, maximum: 500,
-      wrong_length: 'Invalid length of e-mail',
-      too_long: "%{count} characters is the maximum allowed for e-mail"
-      too_short: "e-mail must have at least %{count} characters"
-    }
-  =end
-    , presence: true  
+  validates :email, presence: true     
 end
