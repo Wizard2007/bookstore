23076d2f841931b5af70608a41317b3a706f84d4
Eujine <sponsor85@mail.ru>
2017-03-17 10:02:40 +0200

    [*] fix for heroku deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ApplicationRecord
   has_many :reviews
   validates :first_name, :password, :confirmation, presence: true
   validates :email
-  =begin, length: {
+  =begin , length: {
       minimum: 1, maximum: 500,
       wrong_length: 'Invalid length of e-mail',
       too_long: "%{count} characters is the maximum allowed for e-mail"
       too_short: "e-mail must have at least %{count} characters"
     }
-    end=
+  =end
     , presence: true  
 end
